bd62039a6e17fb09bf053169d4b4f511b0e2e57b
unityweb <info@unity-web.ch>
2012-11-13 15:01:39 +0100

    Cleaning up

--- libraries/prestashop.rb
+++ libraries/prestashop.rb
@@ -1,5 +1,21 @@
 module PrestashopLibrary
 	def prestashop_createuser(username, password)
     		puts "you gave me #{username} and #{password}"
+    end
+	
+
+	def prestashop_fetchrelease()
+	    remote_file "/tmp/prestashop152.zip" do
+  		source "http://www.prestashop.com/download/old/prestashop_1.5.2.0.zip"
+    	mode "0644"
+      	checksum "37aee9ef5388376e4377aeb240ab027e"
+      	backup false
+      	not_if "test -f /tmp/prestashop152.zip"
 	end
+
+	def prestashop_deploy(username) 
+		execute "unzip -o /tmp/prestashop152.zip -d #{node.set['prestashop']['web_folder']}/#{site[1]['username']}" do
+		not_if "test -f /var/www/#{site[1]['username']}/prestashop/index.php"
+	end
+
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -13,21 +13,17 @@ end
 
 
 node["prestashop_sites"].each do |key, site| 
-	Chef::Log.debug("Found a server: name: #{key}, #{site['username']}")
+	
+	Chef::Log.debug("Found a server: name: #{key}, #{site['username']}, #{site['URL']}")
+	
+	# Create User Account on System
 	prestashop_createuser "#{site['username']}", "#{site['password']}"
 	
-#	execute "unzip -o /tmp/prestashop152.zip -d #{node.set['prestashop']['web_folder']}/#{site[1]['username']}" do
-#		not_if "test -f /var/www/#{site[1]['username']}/prestashop/index.php"
-#	end
+	# Fetch the latest release
+	# --TODO ask for version
+	prestashop_fetchrelease ()
+
+	# prestashop_deploysite "#{site['username']}"
+	
+
 end
-
-remote_file "/tmp/prestashop152.zip" do
-  	source "http://www.prestashop.com/download/old/prestashop_1.5.2.0.zip"
-    	mode "0644"
-      	checksum "37aee9ef5388376e4377aeb240ab027e"
-      	backup false
-      	not_if "test -f /tmp/prestashop152.zip"
-end
-
-
-
