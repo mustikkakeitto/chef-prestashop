d87bce5e3c7405d4663bfbede5901ab6a924c12b
unityweb <info@unity-web.ch>
2012-11-13 15:57:24 +0100

    fixing

--- libraries/prestashop.rb
+++ libraries/prestashop.rb
@@ -44,9 +44,9 @@ module PrestashopLibrary
 			owner "root"
 			group node['mysql']['root_group']
 			mode "0600"
-			variables(
+			variables({
 				:username => "#{username}"
-			)
+			})
 			action :create
 		end
 
